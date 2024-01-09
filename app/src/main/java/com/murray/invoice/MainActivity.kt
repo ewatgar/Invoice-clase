291af1bc496a3fb3dc60700889b06ef266a82101
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-01-09 22:14:26 +0100

    preferences 2024-01-09

--- app/src/main/java/com/murray/invoice/MainActivity.kt
+++ app/src/main/java/com/murray/invoice/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.murray.invoice.databinding.ActivityMainBinding
@@ -18,11 +20,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
 
-
+    //Propiedades de acceso al botón flotante de la Activity principal y la barra de herramientas
     val fab: FloatingActionButton get() = binding.fab
     val toolbar: MaterialToolbar get() = binding.toolbar
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
@@ -53,7 +56,9 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                navController.navigate(R.id.settingsFragment)
+                true}
             else -> super.onOptionsItemSelected(item)
         }
     }
